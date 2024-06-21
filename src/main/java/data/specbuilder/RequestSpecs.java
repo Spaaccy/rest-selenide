01368c7a237971f29edf8a7f0656d7dea185525e
Luka Tsinaridze <lukatsinaridze@gmail.com>
2024-06-21 20:37:55 +0400

    add alure

--- src/main/java/data/specbuilder/RequestSpecs.java
+++ src/main/java/data/specbuilder/RequestSpecs.java
@@ -1,5 +1,6 @@
 package data.specbuilder;
 
+import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
@@ -13,12 +14,14 @@ import static data.constants.PetStoreData.PET_STORE_URL;
 public class RequestSpecs {
     public static RequestSpecification getRequestSpecForBookerToken() {
         return new RequestSpecBuilder()
+                .addFilter(new AllureRestAssured())
                 .setBaseUri(BOOKER_BASE_URI)
                 .setContentType(ContentType.JSON)
                 .build();
     }
     public static RequestSpecification getRequestSpecForBooker(String token) {
         return new RequestSpecBuilder()
+                .addFilter(new AllureRestAssured())
                 .setBaseUri(BOOKER_BASE_URI)
                 .setContentType(ContentType.JSON)
                 .addHeader("Cookie", "token=" + token)
@@ -27,6 +30,7 @@ public class RequestSpecs {
 
     public static RequestSpecification getBaseRequestSpecForBooking() {
         return new RequestSpecBuilder()
+                .addFilter(new AllureRestAssured())
                 .setBaseUri(BOOK_STORE_URI)
                 .setContentType(ContentType.JSON)
                 .build();
@@ -35,6 +39,7 @@ public class RequestSpecs {
 
     public static RequestSpecification getBaseRequestSpecForPetStore() {
         return new RequestSpecBuilder()
+                .addFilter(new AllureRestAssured())
                 .setBaseUri(PET_STORE_URL)
                 .setContentType(ContentType.JSON)
                 .build();
@@ -42,6 +47,7 @@ public class RequestSpecs {
 
     public static RequestSpecification getBaseRequestSpecForErgast() {
         return new RequestSpecBuilder()
+                .addFilter(new AllureRestAssured())
                 .setBaseUri(ERGAST_URI)
                 .setContentType(ContentType.JSON)
                 .build();
